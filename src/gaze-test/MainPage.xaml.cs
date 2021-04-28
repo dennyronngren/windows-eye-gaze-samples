57b5a9939b5c24a1ac3476951ae51b75d5c0fc9f
Denny Rönngren <denny.ronngren@gmail.com>
2021-04-28 22:19:59 +0200

    Added basic gaze visualization, fixes #1

--- src/gaze-test/MainPage.xaml.cs
+++ src/gaze-test/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Input.Preview;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +23,96 @@ namespace gaze_test
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private GazeDeviceWatcherPreview _watcher;
+
+        private uint _noofEyeTrackers = 0;
+        private GazeInputSourcePreview _gazeSource;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            HandleEyeTrackingChanged();
+            StartDeviceWatcher();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            StopDeviceWatcher();
+
+            base.OnNavigatedFrom(e);
+        }
+
+        private void StartDeviceWatcher()
+        {
+            _watcher = GazeInputSourcePreview.CreateWatcher();
+            _watcher.Added += OnDeviceAdded;
+            _watcher.Removed += OnDeviceRemoved;
+            _watcher.Updated += OnDeviceUpdated;
+
+            _watcher.Start();
+        }
+
+        private void StopDeviceWatcher()
+        {
+            _watcher.Stop();
+            _watcher = null;
+        }
+
+        private void HandleEyeTrackingChanged()
+        {
+            gazeTrace.Visibility = _noofEyeTrackers > 0 ? Visibility.Visible : Visibility.Collapsed;
+
+            if (_noofEyeTrackers > 0 && _gazeSource == null)
+            {
+                _gazeSource = GazeInputSourcePreview.GetForCurrentView();
+                _gazeSource.GazeMoved += GazeMovedHandler;
+            }
+
+            if (_noofEyeTrackers == 0 && _gazeSource != null) {
+                _gazeSource.GazeMoved -= GazeMovedHandler;
+                _gazeSource = null;
+            }
+        }
+
+        private void OnDeviceAdded(GazeDeviceWatcherPreview sender, GazeDeviceWatcherAddedPreviewEventArgs args)
+        {
+            if (args.Device.CanTrackEyes == true)
+            {
+                _noofEyeTrackers++;
+                HandleEyeTrackingChanged();
+            }
+        }
+
+        private void OnDeviceUpdated(GazeDeviceWatcherPreview sender, GazeDeviceWatcherUpdatedPreviewEventArgs args)
+        {
+            if (args.Device.CanTrackEyes == true)
+            {
+                // do something
+            }
+        }
+
+        private void OnDeviceRemoved(GazeDeviceWatcherPreview sender, GazeDeviceWatcherRemovedPreviewEventArgs args)
+        {
+            if(args.Device.CanTrackEyes == true)
+            {
+                _noofEyeTrackers--;
+                HandleEyeTrackingChanged();
+            }
+        }
+
+        private void GazeMovedHandler(GazeInputSourcePreview sender, GazeMovedPreviewEventArgs args)
+        {
+            var gazePoint = args.CurrentPoint.EyeGazePosition.Value;
+
+            _gazeMarkerTransform.X = gazePoint.X - gazeTrace.ActualWidth / 2;
+            _gazeMarkerTransform.Y = gazePoint.Y - gazeTrace.ActualHeight / 2;
+        }
+
     }
 }
